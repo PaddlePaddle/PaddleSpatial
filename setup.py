141d5e011fa52bf19de6dfa1c0cf6d657551549a
zhoujingbo <zhoujingbo@baidu.com>
2021-12-07 00:32:30 +0800

    finish prepare for pip
    Change-Id: I285fdd239c3f7c1c75bfc2ceade14f19ba1352ea

--- setup.py
+++ setup.py
@@ -12,7 +12,16 @@ Authors: zhoujingbo(zhoujingbo@baidu.com)
 Date:    2021/10/19 10:30:45
 """
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup()
 
+setup(
+    name="paddlespatial",
+    version="0.0.1b",
+    author="baidu-bil",
+    author_email="zhoujingbo@baidu.com",
+    description="Adding version check",
+    zip_safe=False,
+    url='https://github.com/PaddlePaddle/PaddleSpatial',
+    home_page='https://github.com/PaddlePaddle/PaddleSpatial'
+)
